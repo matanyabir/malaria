9182b88c575d8fa1a18491ada5ec09a4c769518c
Matanya Birenboim <matanya@optibus.com>
2020-12-24 21:10:10 +0200

    drag to scroll

--- js/views/mapView.js
+++ js/views/mapView.js
@@ -11,7 +11,7 @@ const MapView = Backbone.View.extend({
 	},
 	render: function ()
 	{
-		this.$mapContainer = $('<div class="map-container"></div>');
+		this.$mapContainer = $('<div id="map-drag-scroll" class="map-container"></div>');
 		this.$bigMap = $('<div class="big-map"></div>');
 		this.$heatMap = $('<div class="heat-map-container"></div>');
 		this.$mapItems = $('<div class="map-items-container"></div>');
@@ -26,6 +26,7 @@ const MapView = Backbone.View.extend({
 		this.renderHouses();
 		this.renderPuddles();
 		this.scrollToMiddle();
+		this.dragToScroll();
 		return this;
 	},
 
@@ -41,6 +42,54 @@ const MapView = Backbone.View.extend({
 			if (verticalScroll > 0) {
 				this.$mapContainer.scrollTop(verticalScroll / 2);
 			}
+		}, 0);
+	},
+	dragToScroll ()
+	{
+		setTimeout( () => {
+			// copy from https://htmldom.dev/drag-to-scroll/
+			// demo: https://github.com/phuoc-ng/html-dom/blob/master/demo/drag-to-scroll/index.html
+			const ele = document.getElementById('map-drag-scroll');
+			ele.style.cursor = 'grab';
+
+			let pos = {top: 0, left: 0, x: 0, y: 0};
+
+			const mouseDownHandler = function (e) {
+				ele.style.cursor = 'grabbing';
+				ele.style.userSelect = 'none';
+
+				pos = {
+					left: ele.scrollLeft,
+					top: ele.scrollTop,
+					// Get the current mouse position
+					x: e.clientX,
+					y: e.clientY,
+				};
+
+				document.addEventListener('mousemove', mouseMoveHandler);
+				document.addEventListener('mouseup', mouseUpHandler);
+			};
+
+			const mouseMoveHandler = function (e) {
+				// How far the mouse has been moved
+				const dx = e.clientX - pos.x;
+				const dy = e.clientY - pos.y;
+
+				// Scroll the element
+				ele.scrollTop = pos.top - dy;
+				ele.scrollLeft = pos.left - dx;
+			};
+
+			const mouseUpHandler = function () {
+				ele.style.cursor = 'grab';
+				ele.style.removeProperty('user-select');
+
+				document.removeEventListener('mousemove', mouseMoveHandler);
+				document.removeEventListener('mouseup', mouseUpHandler);
+			};
+
+			// Attach the handler
+			ele.addEventListener('mousedown', mouseDownHandler);
 		}, 0);
 	},
 	closeGraphView ()
